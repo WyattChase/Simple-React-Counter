98a95f7afa5a4752bdf04f1adb2ddf8474738c7f
WyattChase <esnaruto@yahoo.com>
2023-02-25 00:30:27 +0000

    start up

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SimpleCounter(props) {
+    return (
+        <div className="bigCounter">
+            <div className="clock"><i class="far fa-clock"></i></div>
+            <div className="four"></div>
+            <div className="three"></div>
+            <div className="two"></div>
+            <div className="one"></div>
+        </div>
+    )
+}
+
+export default SimpleCounter
